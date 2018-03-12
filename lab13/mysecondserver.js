0483bf3f0d4085c708307fe159c47bd6d9eeb728
marcusdouglas <marcusdouglas@live.co.uk>
2018-03-12 15:55:22 +0000

    lab13

--- lab13/mysecondserver.js
+++ lab13/mysecondserver.js
@@ -1,8 +1,8 @@
 var http = require("http");
-var currentdata = require("mymodule");
+var currentdata = require("./mymodule");
 
 http.createServer(function (req, res) {
   res.writeHead(200, {"Content-Type": "text/html"});
-  res.write("The date and time are currently: " + currentdata.myDataTime());
+  res.write("The date and time are currently: " + currentdata.myDateTime());
   res.end("Hello World!");
 }).listen(8080);
