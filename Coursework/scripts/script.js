de78b9b54689288623f930ca89567e2a064f8079
marcusdouglas <marcusdouglas@live.co.uk>
2018-03-22 09:57:24 +0000

    Adding Shared CWork Files

--- Coursework/scripts/script.js
+++ Coursework/scripts/script.js
@@ -4,77 +4,8 @@ $(function() {
 
   // Makes sure only the first card image is displayed
   showDivs(slideIndex);
-  //---------------------Changing page content---------------------
-  $("#MainContent").on("click", ".button", function() {
 
-    // Remove current card with fade out and create new one
-    $("#activeCard").fadeOut(500, function() {
-      $("#activeCard").remove();
-
-      addItemToList();
-      $("#activeCard").addClass("card");
-    });
-
-    return false;
-  });
 });
-
-function addItemToList (item) {
-  $("#MainContent").append("<div id = 'activeCard'>" + "<h2 class = 'paraTitle'>Restaurant Name</h2>"
-  + "<div class = 'imageContainer'><img class = 'cardImage' src = 'images/foodImage3.jpeg'/>"
-  + "<img class = 'cardImage' src = 'images/foodImage4.jpeg'/><img class = 'cardImage' src = 'images/foodImage5.jpeg'/>"
-  + "<button id = 'leftButton' class = 'btn' onclick = 'plusDivs(-1)''>&#10094;</button>"
-  + "<button id = 'rightButton' class = 'btn' onclick = 'plusDivs(1)''>&#10095;</button></div>"
-  + "<p>Example Text</p><input id = 'seeMore' class = 'collapseInfo' type = 'checkbox'>"
-  + "<label class = 'collapseLabel' for = 'seeMore'>See more...</label><div class = 'expand'>"
-  + "<p>New Text</p></div><form id = 'scrollForm'><button id = 'dislikeButton' class = 'button' type = 'button'>"
-  + "<button id = 'likeButton' class = 'button' type = 'button'></button></button></form></div>");
-
-  // Makes sure only the first card image is displayed
-  showDivs(slideIndex);
-  $("#activeCard").hide().fadeIn("500");
-}
-
-//---------------------Responsive Nav Bar---------------------
-
-function navCollapse() {
-    var x = document.getElementById("collapseNav");
-    if (x.className === "nav-tabs") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav-tabs";
-    }
-}
-
-//---------------------Sticky Nav Bar---------------------
-
-$(document).ready(function () {
-  $(window).resize(function () {
-      responsiveNav();
-  });
-});
-
-$(document).ready(function () {
-  $(window).scroll(function () {
-    responsiveNav();
-  });
-});
-
-function responsiveNav() {
-  if ($(window).width() > 700) {
-    if ($(window).scrollTop() > 30) {
-      $("#nav").css({marginLeft: "0", width: "100%", marginRight: "0", position: "fixed", top: 0});
-      //$("#MainContent").addClass("sticky");
-    }
-    if($(window).scrollTop() < 330) {
-      $("#nav").css({marginLeft: "15%", width: "70%", marginRight: "15%", position: "static"});
-      //$("#MainContent").removeClass("sticky");
-    }
-  }
-  else {
-    $("#nav").css({marginLeft: "0", width: "100%", marginRight: "0", position: "fixed", top: 0});
-  }
-}
 
 //----------------------Browse Card Images-----------------------
 var slideIndex = 1;
@@ -99,14 +30,55 @@ function showDivs(n) {
   $("#nav").css({zIndex: "+1"});
 }
 
-/*
-$(window).scroll(function () {//30 and 300
-  if ($(window).scrollTop() > 30) {
+//---------------------Responsive Nav Bar---------------------
+
+function navCollapse() {
+    var x = document.getElementById("collapseNav");
+    if (x.className === "nav-tabs") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav-tabs";
+    }
+}
+
+//---------------------Sticky Nav Bar---------------------
+
+$(document).ready(function () {
+  $(window).resize(function () {
+      responsiveNav();
+
+      // Make navigation border radius responsive on resize
+      if ($(window).width() > 700) {
+        $(".nav-tabs").css({borderRadius: "6px"});
+      }
+  });
+});
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    responsiveNav();
+  });
+});
+
+function responsiveNav() {
+  if ($(window).width() > 700) {
+    if ($(window).scrollTop() > 30) {
+      $("#nav").css({marginLeft: "0", width: "100%", marginRight: "0", position: "fixed", top: 0});
+      //$("#MainContent").addClass("sticky");
+    }
+    if($(window).scrollTop() < 330) {
+      $("#nav").css({marginLeft: "15%", width: "70%", marginRight: "15%", position: "static"});
+      //$("#MainContent").removeClass("sticky");
+      $(".nav-tabs").css({borderRadius: "6px"});
+    }
+  }
+  else {
     $("#nav").css({marginLeft: "0", width: "100%", marginRight: "0", position: "fixed", top: 0});
-    $("#MainContent").addClass("sticky");
+    $(".nav-tabs").css({borderRadius: "0"});
   }
-  if($(window).scrollTop() < 300) {
-    $("#nav").css({marginLeft: "15%", width: "70%", marginRight: "15%", position: "static"});
-    $("#MainContent").removeClass("sticky");
-  }
-});*/
+}
+
+// Will remove login form elements visibility
+function loginDisplayNone() {
+  $("#loginForm").css({display: "none"});
+}
