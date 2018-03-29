91dd852718e08d70d2c5f09d3f9268f82f1d104a
marcusdouglas <marcusdouglas@live.co.uk>
2018-03-29 15:24:56 +0100

    cw

--- Coursework/server.js
+++ Coursework/server.js
@@ -19,7 +19,7 @@ app.get("/", function(req, res) {
 });
 
 // about page
-app.get("/about", function(req, res) {
+app.get("/myrestaurants", function(req, res) {
   res.render("pages/myrestaurants");
 });
 
