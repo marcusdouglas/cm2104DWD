3d86617f0e1160947e96caf81ccc72d5e28bf0e3
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-05 17:26:49 +0100

    test

--- TestFolder/script.js
+++ TestFolder/script.js
@@ -7,11 +7,13 @@ $(document).ready(function() {
 
     $.ajax ({
       type: "GET",
+      data: {key: "e3be4f880ddb28c4772c0a42ac6180a5", q: search.slice(0, -1)},
       url: "https://developers.zomato.com/api/v2.1/locations?query=Aberdeen%2C%20Scotland",
       success: function(result) {
         // deal with data here
         console.log(result);
-        $("#resultsbox").append(result);
+        var pretty = JSON.stringify(jsondata, null, 4);
+        $("#resultsbox").append("<pre>" + pretty + "</pre>");
       }
     });
   });
