33aca87ca9e0d6cb5a4281033b7e28d25fafb221
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-07 18:05:39 +0100

    fun

--- fun/server.js
+++ fun/server.js
@@ -1,5 +1,5 @@
 const MongoClient = require("mongodb").MongoClient;
-const url = "mongodb://localhost:27017/star_wars_quotes";
+const url = "mongodb://localhost:27017/swainson_review";
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
