b5a2fa2aa4a6d6b8745cff6e96dfec727223d27c
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-09 16:02:47 +0100

    test

--- TestFolder/cworktest/server.js
+++ TestFolder/cworktest/server.js
@@ -8,7 +8,7 @@ $('retrieve-resources').click(function() {
   $.ajax ({
     url: "https://developers.zomato.com/api/v2.1/locations?query=Aberdeen%2C%20Scotland",
     type: "GET",
-    data: {key: "e3be4f880ddb28c4772c0a42ac6180a5"},
+    headers: {"user-key": "3c672f5af7519d65f72ed90953badca5"},
     dataType: "json",
     success: function(result) {
       // deal with data here
