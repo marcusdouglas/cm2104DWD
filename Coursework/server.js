2799a0c2c5f39a515495f72d9d2a048751f12a2b
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-11 16:54:44 +0100

    cwork

--- Coursework/server.js
+++ Coursework/server.js
@@ -1,11 +1,21 @@
 // sever.js
 // load the things we need
-var express = require("express");
-var app = express();
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017/saved_cards";
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-//set the view engine to EJS
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + '/public'));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+var db;
+
+MongoClient.connect(url, function(err, database) {
+  if (err) throw err;
+  db = database;
+  app.listen(8080);
+});
 
 // use res.render to load up an ejs.view file
 
@@ -26,5 +36,4 @@ app.get("/myrestaurants", function(req, res) {
   res.render("pages/myrestaurants");
 });
 
-app.listen(8080);
 console.log("8080 is the magic port");
