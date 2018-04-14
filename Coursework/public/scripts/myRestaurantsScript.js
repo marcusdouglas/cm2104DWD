6fe35d32146b1a37beafd6fc820603f551137f10
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-14 22:19:20 +0100

    cwork

--- Coursework/public/scripts/myRestaurantsScript.js
+++ Coursework/public/scripts/myRestaurantsScript.js
@@ -1,13 +1,13 @@
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017/saved_cards";
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
 function removeSavedRestaurant() {
 
-  $("#MainContent").on("click", ".savedRestaurant", function() {
-    $(this).fadeOut(500, function() {
-      $(this).remove();
-    });
-  });
-
   $.ajax ({
-    url: "mongodb://localhost:27017/saved_cards",
+    url: url,
     type: "GET",
     dataType: "json",
     success: function(result) {
@@ -15,5 +15,11 @@ function removeSavedRestaurant() {
       console.log(result);
       var res = JSON.parse(JSON.stringify(result));
     }
+  });
+
+  $("#MainContent").on("click", ".savedRestaurant", function() {
+    $(this).fadeOut(500, function() {
+      $(this).remove();
+    });
   });
 }
