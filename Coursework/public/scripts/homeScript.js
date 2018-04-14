d8ae2dc63ec2b9120657ea51ffba522d71c5605e
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-14 17:05:16 +0100

    cwork

--- Coursework/public/scripts/homeScript.js
+++ Coursework/public/scripts/homeScript.js
@@ -178,3 +178,12 @@ function getAverageCost(averageCost) {
 
   return restaurantAverageCost;
 }
+
+function myMap() {
+  var myCenter = new google.maps.LatLng(51.508742,-0.120850);
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {center: myCenter, zoom: 5};
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var marker = new google.maps.Marker({position:myCenter});
+  marker.setMap(map);
+}
