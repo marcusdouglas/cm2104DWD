5bb0d4ccb47f5fbcfc13f10b4a72585c6c6e5c9a
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-14 16:21:03 +0100

    cwork

--- Coursework/server.js
+++ Coursework/server.js
@@ -27,26 +27,19 @@ var app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));*/
 
-// use res.render to load up an ejs.view file
-
-// index page
-//app.get("/", function (req, res) {
-  //res.render("pages/index");
-//});
-
 // index page
 app.get("/", function(req, res) {
   //res.render("pages/index",{pageName:index});
   res.render("pages/index");
 });
 
-// about page
+// myrestaurants page
 app.get("/myrestaurants", function(req, res) {
   //res.render("pages/myrestaurants", {pageName:myrestaurants});
-  //res.render("pages/myrestaurants");
 
   db.collection('card').find().toArray(function(err, result) {
     if (err) throw err;
+
     var restaurants = [];
 
     for (var i = 0; i < result.length; i++) {
@@ -61,24 +54,6 @@ app.get("/myrestaurants", function(req, res) {
       restaurants: restaurants
     });
   });
-/*
-  db.collection('card').find().toArray(function(err, result) {
-    if (err) throw err;
-    var output = "<div class = 'savedRestaurant'>"
-      + "<span class = 'close' onclick = 'removeSavedRestaurant()'>&times;</span>";
-
-    for (var i = 0; i < result.length; i++) {
-      var name = result[i].name;
-      var imageUrl = result[i].image;
-      var text = result[i].text;
-
-      output += "<div class = 'sideImgContainer'>";
-      output += "<img class = 'sideImg' src = " + imageUrl + "></div>";
-      output += "<div class = 'textContainer'><h2 class = 'paraTitle'>" + name + "</h2>";
-      output += "<p class = 'info'>" + text + "</p></div></div>"
-    }
-    res.send(output);
-  });*/
 });
 
 // This works
