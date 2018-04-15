cb590122611af64ae176fa556d3a0fb7387f1a66
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-15 11:57:49 +0100

    cwork

--- Coursework/public/scripts/myRestaurantsScript.js
+++ Coursework/public/scripts/myRestaurantsScript.js
@@ -1,5 +1,11 @@
 function removeSavedRestaurant() {
 
+  var url = "mongodb://localhost:27017/saved_cards";
+
+  $.getJSON(url, function(jsondata) {
+    console.log(jsondata);
+  });
+
   $("#MainContent").on("click", ".savedRestaurant", function() {
     $(this).fadeOut(500, function() {
       $(this).remove();
