487c47ffa1caa017964397c5ac957835054e9b24
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-15 12:41:21 +0100

    cwork

--- Coursework/public/scripts/myRestaurantsScript.js
+++ Coursework/public/scripts/myRestaurantsScript.js
@@ -1,11 +1,3 @@
-var db;
-
-MongoClient.connect(url, function(err, database) {
-  if (err) throw err;
-  db = database;
-  app.listen(8080);
-});
-
 function removeSavedRestaurant() {
   /*
   var url = "mongodb://localhost:27017/saved_cards";
@@ -14,13 +6,23 @@ function removeSavedRestaurant() {
     console.log(jsondata);
   });*/
 
-  db.collection('card').find().toArray(function(err, result) {
-    console.log(result);
-  });
-
   $("#MainContent").on("click", ".savedRestaurant", function() {
+
+    var name = $(this).attr(id);
+
     $(this).fadeOut(500, function() {
       $(this).remove();
     });
+
+    $.ajax({
+         method: "POST",
+         url: "/delete",
+         data: {"name": name},
+         success: function(result) {
+            if(/* check if it is ok */) {
+                location.reload();
+            }
+         }
+      })
   });
 }
