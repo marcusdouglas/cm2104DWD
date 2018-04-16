e092210c1c68b080c13832e34dd3ad150ceac439
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-16 15:42:02 +0100

    cwork

--- Coursework/server.js
+++ Coursework/server.js
@@ -1,7 +1,7 @@
 // sever.js
 // load the things we need
 
-const MongoClient = require("mongodb").MongoClient;
+/*const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb://localhost:27017/saved_cards";
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -17,6 +17,33 @@ MongoClient.connect(url, function(err, database) {
   if (err) throw err;
   db = database;
   app.listen(8080);
+});*/
+
+const MongoClient = require('mongodb').MongoClient; //npm install mongodb@2.2.32
+const url = "mongodb://localhost:27017/profiles";
+const express = require('express'); //npm install express
+const session = require('express-session'); //npm install express-session
+const bodyParser = require('body-parser'); //npm install body-parser
+const app = express();
+
+//this tells express we are using sesssions. These are variables that only belong to one user of the site at a time.
+app.use(session({ secret: 'example' }));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+var db;
+
+
+//this is our connection to the mongo db, ts sets the variable db as our database
+MongoClient.connect(url, function(err, database) {
+  if (err) throw err;
+  db = database;
+  app.listen(8080);
+  console.log('listening on 8080');
 });
 
 // This works
