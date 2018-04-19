494a07946942af3ac1b00f3d1303d4620f2024a9
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-19 15:32:12 +0100

    cwork

--- Coursework/public/scripts/homeScript.js
+++ Coursework/public/scripts/homeScript.js
@@ -150,9 +150,10 @@ function performSearch(entityId, entityType) {
       console.log(result);
       var res = JSON.parse(JSON.stringify(result));
 
+      // These two variables belong to working version
       index = 0;
       restaurantsArray.length = 0;
-
+/*
       //console.log(res.results_found);
       var resultsFound = res.results_found;
       var loop = Math.round(resultsFound / 20);
@@ -182,6 +183,26 @@ function performSearch(entityId, entityType) {
         }
         res.results_start += 20;
         console.log(res.results_start);
+      }*/
+
+      for (var i = 0; i < res.restaurants.length; i++) {
+        var name = res.restaurants[i].restaurant.name;
+        var thumbnail = res.restaurants[i].restaurant.thumb;
+        var userRating = res.restaurants[i].restaurant.user_rating.aggregate_rating;
+        var voteCount = res.restaurants[i].restaurant.user_rating.votes;
+        var foodType = res.restaurants[i].restaurant.cuisines;
+        var averageCost = res.restaurants[i].restaurant.price_range;
+        var siteUrl = res.restaurants[i].restaurant.url;
+        var latitude = res.restaurants[i].restaurant.location.latitude;
+        var longitude = res.restaurants[i].restaurant.location.longitude;
+
+        var restaurant = {name: name, thumbnail: thumbnail,
+          userRating: userRating, voteCount: voteCount,
+          foodType: foodType, averageCost: averageCost, siteUrl: siteUrl,
+          latitude: latitude, longitude: longitude};
+        //console.log(restaurant);
+
+        restaurantsArray[i] = restaurant;
       }
 
       // Shuffle the array. This will help stop the user having to go through
