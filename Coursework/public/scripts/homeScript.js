1fd4b18afbd18cb036546c4a674e01ad4712fd48
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-19 10:54:08 +0100

    cwork

--- Coursework/public/scripts/homeScript.js
+++ Coursework/public/scripts/homeScript.js
@@ -173,9 +173,29 @@ function performSearch(entityId, entityType) {
         restaurantsArray[i] = restaurant;
       }
 
+      shuffle(restaurantsArray);
       createCard();
     }
   });
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
 
 // Uses API data to create a star rating
