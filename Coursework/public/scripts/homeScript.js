df5bbc55580abea4faf36ac31cd85e023072e15d
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-23 00:13:48 +0100

    cwork

--- Coursework/public/scripts/homeScript.js
+++ Coursework/public/scripts/homeScript.js
@@ -289,54 +289,18 @@ function createMap(latitude, longitude) {
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
-  // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
+var searchBox;
+   function initAutocomplete() {
 
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    var places = searchBox.getPlaces();
+   var options = {types: ['(cities)']};
 
-    if (places.length == 0) {
-      return;
-    }
+   var input = document.getElementById('placeAuto');
+    searchBox = new google.maps.places.Autocomplete(input);
+   searchBox.addListener('place_changed', fillInAddress);
+   }
 
-    // Clear out the old markers.
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers = [];
-
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      if (!place.geometry) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
-
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
+  function fillInAddress()
+  {
+      var place = searchBox.getPlace();
+      console.log(place);
+  }
