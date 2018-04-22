885b0d2e9a18eb5dafba91522ba3548dae11ec93
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-23 00:22:11 +0100

    cwork

--- Coursework/public/scripts/homeScript.js
+++ Coursework/public/scripts/homeScript.js
@@ -281,15 +281,7 @@ function createMap(latitude, longitude) {
   marker.setMap(map);
 }
 
-// This example adds a search box to a map, using the Google Place Autocomplete
-// feature. People can enter geographical searches. The search box will return a
-// pick list containing a mix of places and predicted search terms.
-
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
 function initialize() {
-  var input = document.getElementById('searchTextField');
+  var input = $("#location");
   new google.maps.places.Autocomplete(input);
 }
