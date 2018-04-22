31880570270880dbf4a4250d04422182ee4b1326
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-22 16:55:54 +0100

    cwork

--- Coursework/server.js
+++ Coursework/server.js
@@ -227,7 +227,7 @@ app.post('/login', function(req, res) {
     if (err) throw err;//if there is an error, throw the error
     //if there is no result, redirect the user back to the login system as that username must not exist
     if(!result){
-      //res.redirect('/');
+      res.redirect('/');
       return;
     }
     //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
