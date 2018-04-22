e28211249841e8264eb4c81a3d17a836adcf5580
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-22 20:49:35 +0100

    cwork

--- Coursework/public/scripts/myRestaurantsScript.js
+++ Coursework/public/scripts/myRestaurantsScript.js
@@ -19,27 +19,3 @@ function removeSavedRestaurant() {
       });
   });
 }
-
-$(function() {
-  getAverage();
-});
-
-function getAverage() {
-  var restaurantAverageCost = "";
-  var poundCount = 0;
-  averageCost = <%= restaurant.averageCost %>;
-
-  for (var i = 0; i < averageCost; i++) {
-    restaurantAverageCost += "<span class='fa fa-dollar dollarOn'></span>";
-    poundCount++;
-  }
-
-  for (var i = poundCount; i < 4; i++) {
-    restaurantAverageCost += "<span class='fa fa-dollar dollarOff'></span>";
-    poundCount++;
-  }
-
-  console.log(restaurantAverageCost);
-
-  $("#costText").append(restaurantAverageCost);
-}
