6571d959f6cb09587d61e26cb75e6fe3b97705b3
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-23 00:18:50 +0100

    cwork

--- Coursework/public/scripts/homeScript.js
+++ Coursework/public/scripts/homeScript.js
@@ -293,5 +293,3 @@ function initialize() {
   var input = document.getElementById('searchTextField');
   new google.maps.places.Autocomplete(input);
 }
-
-google.maps.event.addDomListener(window, 'load', initialize);
