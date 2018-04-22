ea69351a37701034975b0c49585c29aa42538e5e
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-23 00:05:59 +0100

    cwork

--- Coursework/public/scripts/homeScript.js
+++ Coursework/public/scripts/homeScript.js
@@ -289,22 +289,9 @@ function createMap(latitude, longitude) {
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
-function initAutocomplete() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.8688, lng: 151.2195},
-    zoom: 13,
-    mapTypeId: 'roadmap'
-  });
-
   // Create the search box and link it to the UI element.
   var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
 
   var markers = [];
   // Listen for the event fired when the user selects a prediction and retrieve
