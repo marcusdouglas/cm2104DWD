e4eebca9b4c882a5b09cd30729cc3ece890bf789
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-24 16:35:37 +0100

    cwork

--- Coursework/public/scripts/homeScript.js
+++ Coursework/public/scripts/homeScript.js
@@ -12,7 +12,7 @@ $(function() {
 //---------------------Creates a new card---------------------
   $("#MainContent").on("click", ".button", function() {
 
-    if (index === restaurantsArray.length - 1) {
+    if (index === restaurantsArray.length) {
       $("#cardLoader").addClass("loader");
       getLocation();
     }
