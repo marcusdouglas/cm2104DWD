53ef605a69ffb18f81fcb3297ae1d688fbf36cb3
marcusdouglas <marcusdouglas@live.co.uk>
2018-04-26 20:02:06 +0100

    cwork

--- Coursework/public/scripts/myRestaurantsScript.js
+++ Coursework/public/scripts/myRestaurantsScript.js
@@ -28,7 +28,7 @@ function removeSavedRestaurant() {
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         document.getElementById("topButton").style.display = "block";
     } else {
         document.getElementById("topButton").style.display = "none";
